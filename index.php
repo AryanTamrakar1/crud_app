cc10fcd84eb464776a934fdfebef6f9036e08a21
AryanTamrakar1 <aryan.tamrakar98@gmail.com>
2026-07-07 17:31:01 +0545

    Initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php include('header.php'); ?>
+<h2>ALL STUDENTS</h2>
+<table class="table table-hover table-bordered table-striped">
+    <thread>
+        <tr>
+            <th>
+                ID
+            </th>
+            <th>
+                FIRST NAME
+            </th>
+            <th>
+                LAST NAME
+            </th>
+            <th>
+                AGE
+            </th>
+        </tr>
+    </thread>
+    <tbody>
+        <tr>
+            <td>
+                3
+            </td>
+            <td>
+                Anirban
+            </td>
+            <td>
+                Bhowmick
+            </td>
+            <td>
+                23
+            </td>
+        </tr>
+        <tr>
+            <td>
+                5
+            </td>
+            <td>
+                Rohit
+            </td>
+            <td>
+                Singh
+            </td>
+            <td>
+                26
+            </td>
+        </tr>
+    </tbody>
+</table>
+<?php include('footer.php'); ?>
